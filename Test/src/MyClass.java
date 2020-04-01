2de64a29ee189356a6483cfe7c13c537971e3097
Nils Maier <nils.maier@accenture.com>
2020-04-01 11:26:16 +0200

    hallo

---
+++ Test/src/MyClass.java
@@ -0,0 +1,15 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello WOrld");
+		boolean A=false;
+if (A) {
+	System.out.println("hello wooorld");
+}
+else {
+	System.out.println("no hello");
+}
+	}
+
+}
